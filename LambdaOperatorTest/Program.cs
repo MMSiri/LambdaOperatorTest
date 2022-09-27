f3a93d47d632a42fb1dde572f4c7924a174db6f7
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-09-27 18:53:34 +0100

    Add project files.

---
+++ LambdaOperatorTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LambdaOperatorTest
+{
+    class Program
+    {
+        static Random random = new Random();
+
+
+        /*static double GetRandomDouble(int max)
+        {
+            return max * random.NextDouble();
+        }*/
+
+        static double GetRandomDouble(int max) => max * random.NextDouble();
+
+        /*static void PrintValue(double d)
+        {
+            Console.WriteLine($"The value is {d:0.0000}");
+        }*/
+
+        static void PrintValue(double d) => Console.WriteLine($"The value is {d:0.0000}");
+
+        static void Main(string[] args)
+        {
+            var value = Program.GetRandomDouble(100);
+            Program.PrintValue(value);
+        }
+    }
+}
